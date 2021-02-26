26e464ad0d5e66fbae3a6d7b8270a81a212d9716
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2021-02-26 00:21:36 -0300

    Removed handlebars, started Auth Middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const handlebars = require('express-handlebars');
 const routes = require('./routes');
 const locate = require('./util/locate');
 
@@ -8,10 +7,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-app.engine('hbs', handlebars({ extname: '.hbs', defaultLayout: 'main' }));
-app.set('view engine', 'hbs');
-app.set('views', locate('src/views'));
 
 app.use(routes);
 
--- src/controllers/Home.js
+++ src/controllers/Home.js
@@ -1,5 +1,3 @@
 module.exports = function (_, response) {
-    return response.render('home', {
-       title: 'Título maneiro' 
-    });
+    return response.json({ status: 'Ok' });
 }
